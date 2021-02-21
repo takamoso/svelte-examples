6a059efab8759e6f2d82543cf4c07094ec3ff9ab
takamoso <htmlcssjslove@gmail.com>
2021-02-21 11:16:30 +0900

    Add hello-world-babel

---
+++ src/hello-world-babel/App.svelte
@@ -0,0 +1,14 @@
+<h1>{ map.get('first') } { map.get('second') }!</h1>
+
+<script>
+  const map = new Map()
+
+  map.set('first', 'Hello')
+  map.set('second', 'world')
+</script>
+
+<style>
+  h1 {
+    color: #ff3e00;
+  }
+</style>
