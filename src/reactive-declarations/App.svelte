e9cd90a68e602057508fab742ce932c9655c6265
takamoso <htmlcssjslove@gmail.com>
2021-02-23 12:00:55 +0900

    Add reactive-declarations

---
+++ src/reactive-declarations/App.svelte
@@ -0,0 +1,10 @@
+<button on:click={ () => count++ }>Count: { count }</button>
+<p>{ count } * 2 = { double }</p>
+<p>{ double } * 2 = { quadruple }</p>
+
+<script>
+  let count = 1
+  
+  $: double = count * 2
+  $: quadruple = double * 2
+</script>
