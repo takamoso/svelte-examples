367610dfd01fbbdf1fe0b6822acc725122baf018
takamoso <htmlcssjslove@gmail.com>
2021-02-24 13:03:58 +0900

    Add numeric-inputs

---
+++ src/numeric-inputs/App.svelte
@@ -0,0 +1,14 @@
+<label>
+  <input type="number" bind:value={ a } min="0" max="10">
+  <input type="range" bind:value={ a } min="0" max="10">
+</label>
+<label>
+  <input type="number" bind:value={ b } min="0" max="10">
+  <input type="range" bind:value={ b } min="0" max="10">
+</label>
+<p>{ a } + { b } = { a + b }</p>
+
+<script>
+  let a = 1
+  let b = 2
+</script>
